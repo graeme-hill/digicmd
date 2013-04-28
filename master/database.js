b388a2c9d77a6722b0887bcaa1a28a55038d5212
Graeme Hill <graemekh@gmail.com>
2013-04-27 17:36:49 -0700

    checkpoint

---
+++ master/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+	db: {
+		slaves: [],
+		users: [
+			{
+				username: "graeme",
+				password: "foobar",
+				email: "graemekh@gmail.com",
+				name: "Graeme Hill"
+			}
+		]
+	}
+};
